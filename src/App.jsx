26c5de8aa3258bfd3bb4bfd48a698d030b3eb372
AlexJhonathan <alexjhonathan04tj@gmail.com>
2026-05-07 03:03:45 -0400

    chore: App.jxs (rutas basicas), axiosInstance, medicoService

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 import './App.css'
 import './index.css'
 
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+// Componentes ficticios temporales
+const Inicio = () => <div>Inicio</div>
+const Buscar = () => <div>Buscar Médicos</div>
+const MisTurnos = () => <div>Mis Turnos</div>
+const Agenda = () => <div>Agenda Médico</div>
+
+// Rutas definidas
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/inicio" element={<Inicio />} />
+      <Route path="/buscar" element={<Buscar />} />
+      <Route path="/mis-turnos" element={<MisTurnos />} />
+      <Route path="/agenda" element={<Agenda />} />
+
+      {/* Ruta por defecto */}
+      <Route path="/" element={<Navigate to="/inicio" />} />
+    </Routes>
+  )
+}
+
 function App() {
   return (
     <div className="app-container">
